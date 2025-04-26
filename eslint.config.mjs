a3e88dfbd5d99c56e48703b1daa005bd6c0a83d1
ajosecortes <alfonsochavarro@flare.com.co>
2025-04-26 16:08:18 -0500

    Add initial configuration files and dependencies for project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,37 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "no-console": "warn",
+      "jsx-quotes": ["warn", "prefer-double"],
+      "semi": [2, "always"],
+      "space-before-function-paren": ["off"],
+      "camelcase": [
+        "error",
+        {
+          "allow": [
+            "api_url",
+            "some_other_identifier"
+          ]
+        }
+      ],
+      "no-unused-vars": "off",
+      "comma-dangle": [
+        "error",
+        {
+          "functions": "never"
+        }
+      ]
+    }
+  }
 ];
 
 export default eslintConfig;
